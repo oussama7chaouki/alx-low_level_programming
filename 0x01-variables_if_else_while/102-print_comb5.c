7545722a309591f8d508b8487d2474cf3f9bfd78
ALX OUSSAMA <oussama11chaouki@gmail.com>
2023-08-11 12:34:13 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'prints all possible combinations of two two-digit numbers'
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i, j, l, k;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 9 ; j++)
+		{
+			for (l = i ; l < 10 ; l++)
+			{
+				for (k = j+1 ; k < 10 ; k++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(l + '0');
+					putchar(k + '0');
+					if (i == 9 && j == 8 && l == 9 && k == 9)
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

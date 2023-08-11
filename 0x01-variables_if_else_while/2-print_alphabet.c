341d1af2b8ab4a7bbb2df4f08f7b3144228aed38
ALX OUSSAMA <oussama11chaouki@gmail.com>
2023-08-11 10:37:02 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'print the last digit of the number stored in the variable n'
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}

2a2bf23e802280301f68499b275dd31dc5163660
oussama alx linux <oussama11chaouki@gmail.com>
2023-08-16 00:56:27 +0100

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	long int i, prev1, prev2, prev3, sum;
+	long int prev1, prev2, prev3, sum;
 
 	sum = 0;
 	prev1 = 1;
 	prev2 = 2;
 	while (prev1 < 4000000)
 	{
-		if(prev1 % 2 == 0)
-			sum +=prev1;
+		if (prev1 % 2 == 0)
+			sum += prev1;
 		prev3 = prev2;
 		prev2 = prev1 + prev2;
 		prev1 = prev3;

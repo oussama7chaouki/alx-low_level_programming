47d10d3b9341cb65c076e5a73306d20a0fe91586
oussama alx linux <oussama11chaouki@gmail.com>
2023-08-16 01:00:40 +0100

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+/**
+ * main - print all natural numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int i, prev1, prev2, prev3;
+
+	prev1 = 1;
+	prev2 = 2;
+	for (i = 0; i < 99; i++)
+	{
+		printf("%lu", prev1);
+		if (i != 98)
+			printf(", ");
+		prev3 = prev2;
+		prev2 = prev1 + prev2;
+		prev1 = prev3;
+	}
+	printf("\n");
+	return (0);
+}

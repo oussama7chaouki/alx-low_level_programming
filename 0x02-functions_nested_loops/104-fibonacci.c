64fd45d781ea66054d7014277c8a994af3ac33f7
oussama alx linux <oussama11chaouki@gmail.com>
2023-08-16 02:10:36 +0100

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,18 +6,41 @@
  */
 int main(void)
 {
-	long int i, prev1, prev2, prev3;
+	int i;
+	unsigned long prev1, prev2, sum;
+	unsigned long prev1_half1, prev1_half2, prev2_half1, prev2_half2;
+	unsigned long half1, half2;
 
-	prev1 = 1;
-	prev2 = 2;
-	for (i = 0; i < 99; i++)
+	prev1 = 0;
+	prev2 = 1;
+	for (i = 0; i < 92; i++)
 	{
-		printf("%lu", prev1);
+		sum = prev1 + prev2;
+		printf("%lu", sum);
+		printf(", ");
+		prev1 = prev2;
+		prev2 = sum;	
+	}
+	prev1_half1 = prev1 / 10000000000;
+	prev2_half1 = prev2 / 10000000000;
+	prev1_half2 = prev1 % 10000000000;
+	prev2_half2 = prev2 % 10000000000;
+	for (i = 93; i < 99; i++)
+	{
+		half1 = prev1_half1 + prev2_half1;
+		half2 = prev1_half2 + prev2_half2;
+		if (prev1_half2 + prev2_half2 > 9999999999)
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
+		printf("%lu%lu", half1, half2);
 		if (i != 98)
 			printf(", ");
-		prev3 = prev2;
-		prev2 = prev1 + prev2;
-		prev1 = prev3;
+		prev1_half1 = prev2_half1;
+		prev1_half2 = prev2_half2;
+		prev2_half1 = half1;
+		prev2_half2 = half2;
 	}
 	printf("\n");
 	return (0);

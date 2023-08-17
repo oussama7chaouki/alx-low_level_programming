51992bc9bdbc39cda588ebd94e7043d3e114625c
oussama alx linux <oussama11chaouki@gmail.com>
2023-08-17 12:58:48 +0100

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a:first digit
+ * @b:second digit
+ * Return: 1 if uppercase 0 otherwise
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

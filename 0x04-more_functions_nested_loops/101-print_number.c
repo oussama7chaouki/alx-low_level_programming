01fb6c45f8c4547ec536c63cbdd6b180ce6ca993
oussama alx linux <oussama11chaouki@gmail.com>
2023-08-18 01:58:44 +0100

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,15 @@
  */
 void print_number(int n)
 {
-	int rec = n;
+	unsigned int rec ;
 
+	rec = n;
 	if (n < 0)
 	{
 		_putchar(45);
 		rec = -rec;
 	}
 	if (rec / 10)
-	{
 		print_number(rec / 10);
-	}
 	_putchar('0' + rec % 10);
 }

9aa4d79d01b8e73764b75b1c43d2e2335add6f76
oussama alx linux <oussama11chaouki@gmail.com>
2023-08-23 18:26:13 +0100

    task 7

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string
+ * Return: capitalizes string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0, j, l = 0;
+	char sep[] = " \t\n{}(),;.!?\"";
+
+	while (str[i] != '\0')
+	{
+		for (l = 0; l < 12; l++)
+		{
+			if (str[i] == sep[l])
+			{
+				j = str[i + 1];
+				if (j >= 97 && j <= 122)
+				{
+					j -= 32;
+					str[i + 1] = j;
+				}
+			}
+		}
+		i++;
+	}
+	return (str);
+}

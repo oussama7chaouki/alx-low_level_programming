5dc4ae1c36c856230f65c89533151271bc367b1b
oussama alx linux <oussama11chaouki@gmail.com>
2023-08-28 19:27:42 +0100

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_strpbrk - searches a string for any of a set of bytes
+ * @s: the buffer
+ * @accept: substring
+ * Return: Returns a pointer to the byte in s that matches one of the bytes
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				return (s + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}

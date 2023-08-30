1556e23d0adb3585d7bd9753d74112adb0cb6d57
oussama alx linux <oussama11chaouki@gmail.com>
2023-08-30 12:18:39 +0100

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion -  returns the length of a string.
+ * @s: string
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s != '\0')
+	{
+		length = 1 + _strlen_recursion(s + 1);
+	}
+		return (length);
+}

b0643fe82358add66f9d61b6d9a642c4e6852c87
oussama alx linux <oussama11chaouki@gmail.com>
2023-08-30 18:59:03 +0100

    task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * compare_strings - determine if two strings are a match.
+ * @string1: the first string.
+ * @string2: the second string.
+ * @index1: current index in the first string.
+ * @index2: current index in the second string. (wildcard)
+ * Return: 1 if both strings match, 0 otherwise.
+ */
+int compare_strings(char *string1, char *string2, int index1, int index2)
+{
+	if (string1[index1] == '\0' && string2[index2] == '\0')
+		return (1);
+	if (string1[index1] == string2[index2])
+	return (compare_strings(string1, string2, index1 + 1, index2 + 1));
+	if (string1[index1] == '\0' && string2[index2] == '*')
+		return (compare_strings(string1, string2, index1, index2 + 1));
+	if (string2[index2] == '*')
+		return (compare_strings(string1, string2, index1 + 1, index2) ||
+			compare_strings(string1, string2, index1, index2 + 1));
+	return (0);
+}
+
+/**
+ * wildcard_match - determine if two strings match considering wildcards.
+ * @str1: the first string.
+ * @str2: the second string.
+ * Return: 1 if strings match, taking wildcards into account.
+ */
+int wildcard_match(char *str1, char *str2)
+{
+	return (compare_strings(str1, str2, 0, 0));
+}

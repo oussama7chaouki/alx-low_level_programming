f8a15fdff9ac02537e89438c3f2872531405f80d
oussama alx linux <oussama11chaouki@gmail.com>
2023-09-04 14:12:33 +0100

    project 0x0B

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,10 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	arr = (char *)malloc(sizeof(char) * size);
-	if (arr == NULL)
+	array = (char *)malloc(sizeof(char) * size);
+	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		arr[i] = c;
-	return (arr);
+		array[i] = c;
+	return (array);
 }

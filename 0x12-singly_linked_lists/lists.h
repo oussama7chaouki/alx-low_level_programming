ff2a67cedf22387f0a2509f1b0f8164c31f08f16
oussama alx linux <oussama11chaouki@gmail.com>
2023-09-21 22:11:31 +0100

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,6 +28,5 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-int _strlen(const char *s);
 
 #endif

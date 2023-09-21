b4486c92337c55a0450ef3323df9fdfc1618de38
oussama alx linux <oussama11chaouki@gmail.com>
2023-09-21 21:55:30 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		i++;
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);

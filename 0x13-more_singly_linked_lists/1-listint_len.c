72f4c7019ecabe28d386807487e50a1fce7d6e0c
oussama alx linux <oussama11chaouki@gmail.com>
2023-09-25 11:15:34 +0100

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: head of linklist node
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}

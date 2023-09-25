69db4b3da6ab7f14f9e7928483c7e13fbb56fb44
oussama alx linux <oussama11chaouki@gmail.com>
2023-09-25 11:28:17 +0100

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees a linkedlist
+ * @head: the head of the list
+ * Return: NULL if error
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	if (head == NULL)
+		return (NULL);
+	while (head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+}
